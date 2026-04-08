10bf391cc91113d0ce864610487b5c1cea514e83
ezra.fa <ezra.fa@centraltest.com>
2026-04-08 12:12:56 +0200

    initial commit

---
+++ src/data/songs.ts
@@ -0,0 +1,211 @@
+export const songs = [
+  {
+    "id": "1770486648522",
+    "parts": [
+      {
+        "type": "verse",
+        "content": "Misaotra Anao izahay\nRy Tompo tia anay\nFa mbola nomenao\nTombon’andro hahay midera Anao\nNy zavatra ataonay, ankinina Aminao\nFa lehibe tokoa ny fitiavanao\nNy mpanomponao",
+        "id": "1770486449007"
+      },
+      {
+        "type": "chorus",
+        "content": "Hery lehibe tokoa ho anay\nNy mivavaka Aminao\nKoa ampio ny olonao\nIzay mangataka Aminao\nNy fahasoavanao",
+        "id": "1770486640583"
+      }
+    ],
+    "categories": ["Fiankohofana", "Fisaorana"],
+    "createdAt": 1770486648522,
+    "title": "Misaotra anao izahay",
+    "updatedAt": 1770549840667,
+    "number": 1,
+    "playbackLink": "",
+    "songLink": ""
+  },
+  {
+    "id": "1770486730677",
+    "playbackLink": "",
+    "songLink": "",
+    "parts": [
+      {
+        "content": "Haleloia ho an’Ilay Mpanjaka\nHaleloia ho an’ny Tompon’ny Tompo\nMendrika Izy (3)\nHaleloia ho an’Ilay Mpanjaka\nHaleloia ho an’ny Tompon’ny Tompo",
+        "type": "verse",
+        "id": "1770486656514"
+      },
+      {
+        "content": "Ny fanatrehan’ny Mpanjaka\nMisy fifaliana mandrakizay\n‘ndao hanandram-peo sy hihira\nVoninahitra sy ny dera\nFankalazana fa Mpanjakatsika",
+        "id": "1770486703414",
+        "type": "verse"
+      }
+    ],
+    "createdAt": 1770486730677,
+    "updatedAt": 1770486730677,
+    "title": "Haleloia ho an'ilay Mpanjaka",
+    "number": 2,
+    "categories": ["Fiankohofana"]
+  },
+  {
+    "id": "1770486859384",
+    "playbackLink": "",
+    "parts": [
+      {
+        "id": "1770486734334",
+        "type": "verse",
+        "content": "Andriananahary ao an-danitra ao\nNy zava-boahary mihira ho Anao\nTsy misalasala fa notiavinao\nFa mahalala ny fitahianao"
+      },
+      {
+        "type": "verse",
+        "id": "1770486774882",
+        "content": "Ambarao ny hatsarany\nFa mihavao no tsy mety lany\nDia hahita ny famindram-pony isika\nHahita ny famindram-pony"
+      },
+      {
+        "id": "1770486835332",
+        "type": "chorus",
+        "content": "Fa tsara, fa tsara Izy (4)"
+      },
+      {
+        "content": "Tsy tratry the saina ny fahamarinanao\nTsy hay lazaina\nna ny fahamasinanao\nFa Mpiandry tsara mitady ny very\nMpikarakara Rainay mahery",
+        "id": "1770486851161",
+        "type": "verse"
+      }
+    ],
+    "songLink": "",
+    "createdAt": 1770486859384,
+    "updatedAt": 1770486859384,
+    "number": 3,
+    "title": "Andriananahary",
+    "categories": ["Fiankohofana"]
+  },
+  {
+    "id": "1770486917486",
+    "categories": ["Fiankohofana", "Fanoloran-tena"],
+    "number": 4,
+    "createdAt": 1770486917486,
+    "songLink": "",
+    "parts": [
+      {
+        "content": "Velomy ny fitiavako Anao\nAmpireheto aho\nKa ny fahitako, hitoetra ao\nAmpireheto aho",
+        "id": "1770486867191",
+        "type": "verse"
+      },
+      {
+        "type": "chorus",
+        "id": "1770486900194",
+        "content": "Ny fitia volamena, voadio Aminao\nMba arotsay, hameno anay\nTompo ô! Ilay fitiavanao\nOmeo fahamarinana aho\nAmpireheto aho"
+      },
+      {
+        "content": "Fantatrao ny foko sy (ny) fiainako\nAmpireheto aho\nAhitsio aho Tompo ô!\nMiantso Anao\nAmpireheto aho",
+        "id": "1770486906083",
+        "type": "verse"
+      }
+    ],
+    "title": "Ampireheto aho",
+    "playbackLink": "",
+    "updatedAt": 1770486917487
+  },
+  {
+    "id": "1770487023856",
+    "title": "Mifankatiava (vao)",
+    "number": 5,
+    "updatedAt": 1770487023856,
+    "createdAt": 1770487023856,
+    "playbackLink": "",
+    "songLink": "",
+    "categories": ["Fiderana"],
+    "parts": [
+      {
+        "id": "1770486927523",
+        "content": "Mifankatiava e!e!\nMifankatiava a!a!\nCouplet (2x)",
+        "type": "chorus"
+      },
+      {
+        "id": "1770486972116",
+        "content": "Namela fianarana ho antsika Jesosy\nNy amin’ny fitiavana\nFitiavana tsy mba manavakavaka e!\nFitiavana fitiavana",
+        "type": "extra"
+      },
+      {
+        "content": "Mifankativa e!e!e!e! (8)",
+        "type": "chorus",
+        "id": "1770486988257"
+      },
+      {
+        "content": "Mitaky fitiavana amitsika Jesosy\nRaha ho avy Izy\nNy olona manana an’io fitiavana io\nNo afaka hitsena Azy",
+        "id": "1770486999320",
+        "type": "verse"
+      },
+      {
+        "type": "chorus",
+        "id": "1770487001400",
+        "content": "Mifankativa e!e!e!e! (8)"
+      },
+      {
+        "id": "1770487002102",
+        "type": "verse",
+        "content": "Tarigetra ho tratrarina\nDia the hiakatra any an-danitra\nKa nahoana raha mifankatia\nFa ilaozana anie ity tany ity\nSa ahoana e!\nAndao ary e!"
+      }
+    ]
+  },
+  {
+    "id": "1770622237704",
+    "parts": [
+      {
+        "id": "1770622120190",
+        "type": "verse",
+        "content": "Ho an’ilay mipetraka\nEo ambonin’ny seza fiandrianana\nSy ho an’ny Zanak’ondry anie\nNy saotra sy ny haja\nSy ny voninahitra\nAry ny fanjakana mandrakizay\nMandrakizay"
+      },
+      {
+        "content": "Ny saotra sy ny haja\nSy ny voninahitra\nAry ny fanjakana mandrakizay\nMandrakizay\nHo an’Andriamanitra\nSy ny Zanak’ondry\nMandrakizay, mandrakizay\nAmen",
+        "id": "1770622196032",
+        "type": "verse"
+      }
+    ],
+    "categories": ["Fiderana"],
+    "updatedAt": 1770622237705,
+    "playbackLink": "",
+    "title": " Ho an'ilay mipetraka ",
+    "songLink": "",
+    "number": 527,
+    "createdAt": 1770622237704
+  },
+  {
+    "id": "1770625221440",
+    "parts": [
+      {
+        "type": "verse",
+        "content": "Toa mirodana ny tontolonao\nMaizana ny lalanao\nNy manodidina toa manaso anao\n\"AIZA NY ANDRIAMANITRAO\"\nAza mbola kivy fa tsy tapitra Ny story\nNamako o!\nYou dan't have to worry",
+        "id": "1770623053098"
+      },
+      {
+        "id": "1770623362768",
+        "type": "verse",
+        "content": "NO MORE\nefa Tonga ny NGEZA \nhiady ny adinao\nKa matokia fa....\n"
+      },
+      {
+        "type": "chorus",
+        "id": "1770623626967",
+        "content": "Mpanao lalana  IZY \nmpanao lalana,\nTsy azo ferana IZY )\nJesosy no Andriamanitra\nMpanao lalana  IZY \nmpanao lalana,\nTsy taitra tsy maika fa tsy tara \n(Jesosy no Andriamanitra)\n"
+      },
+      {
+        "id": "1770623626983",
+        "content": "Lazarosy aza efa maty hafarana\nRehefa tonga the Tompo \nfiandohana ny fiafarana\nNy manodidina \nhiara hiaiky aminao\n(Jesosy no Andriamanitra)\nAza mbola kivy fa tsy tapitra Ny Story\nNamako o!\nYou dan't have to worry\nNO MORE\n\n",
+        "type": "verse"
+      },
+      {
+        "content": "Efa tonga ny NGEZA hiady nyadina\nka matokia fa.......\n",
+        "type": "verse",
+        "id": "1770623887031"
+      },
+      {
+        "id": "1770623889871",
+        "content": "Mpanao lalana  IZY \nmpanao lalana,\nTsy azo ferana IZY )\nJesosy no Andriamanitra\nMpanao lalana  IZY \nmpanao lalana,\nTsy taitra tsy maika fa tsy tara \n(Jesosy no Andriamanitra)2x",
+        "type": "chorus"
+      }
+    ],
+    "number": 528,
+    "songLink": "https://www.youtube.com/watch?v=7LcvQSETkc8",
+    "title": "Mpanao lalana ",
+    "categories": ["Fiderana"],
+    "playbackLink": "",
+    "createdAt": 1770625221440,
+    "updatedAt": 1770625221440
+  }
+];
